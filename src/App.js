5f3ff5a74901eb03f2196321904274d11231a29a
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-11-27 23:50:11 +0530

    build deploy V1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,} from 'react-router-dom';
 import Homepage from './components/Homepage';
 import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename='/demo'>
       <Routes>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/About' component={About} />
         <Route exact path='/Contact' component={Contact} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
