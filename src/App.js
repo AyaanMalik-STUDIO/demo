0ca9ade3787547e74657cd7fd668d6f4c0e86bb3
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-11-28 13:38:43 +0530

    error solving V1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route,} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
 import Homepage from './components/Homepage';
 import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-     <Homepage/>
+    <Router basename='/demo'>
       <Routes>
-        <Route path='/' component={Homepage} />
-        <Route path='/About' component={About} />
-        <Route path='/Contact' component={Contact} />
+        <Route path='/demo' element={<Homepage/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
       </Routes>
     </Router>
   );
